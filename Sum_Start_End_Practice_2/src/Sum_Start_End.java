56901e783c609591f07496aa2738864bae1a3295
jiayu-yang253 <3443661989@qq.com>
2026-09-25 16:41:50 +0800

    practice2

--- Sum_Start_End_Practice_2/src/Sum_Start_End.java
+++ Sum_Start_End_Practice_2/src/Sum_Start_End.java
@@ -1,27 +1,3 @@
-# Java-Programming
-lecture and practice
-
-practice1
-task1
-1.down load IDE and setup to D:\JIAYU ��ѧ\IntelliJ IDEA 2026.2.2
-
-task2
-2.create github account
-3.setup Git-2.18.0-64-bit.exe and TortoiseGit-2.6.0.0-64bit.msi, and config ssh 
-4.new repository Java Programming https://github.com/jiayu-yang253/Java-Programming
-5.new branches lecture and practice
-
-6.new java class, then printIo Hello world! to console, pls refer to the guideline
-
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
-
-practice2
-repeate practice6 step6
-task1
 import java.util.Scanner;
 public class Sum_Start_End {
     public static void main(String[] args) {
@@ -42,7 +18,3 @@ public class Sum_Start_End {
         scanner.close();
     }
 }
-
-task2 
-scanner end < start, report error
-
